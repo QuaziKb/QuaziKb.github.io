371f60b514538eb741def50f58d5064acf922af0
Kevin B <Quazi.kb@gmail.com>
2021-03-01 12:55:49 -0500

    bugfix
    bugfix

--- WaveEq/js/main.js
+++ WaveEq/js/main.js
@@ -307,11 +307,11 @@ if(true){
     env_mode.slider.value = 4;
     env_mode.slider.oninput();
     });
-    var image4 = new Image();
+    var image5 = new Image();
     image5.src = "texture5.png";
     image5.addEventListener('load', function() {
     gl.activeTexture(gl.TEXTURE0);
-    gl.bindTexture(gl.TEXTURE_2D, texture4);
+    gl.bindTexture(gl.TEXTURE_2D, texture5);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,gl.UNSIGNED_BYTE, image5);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
